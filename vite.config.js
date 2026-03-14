ba83ddccd239ae06c46380f1924911ed169e3790
amterutuja30-wq <amterutuja30@gmail.com>
2026-03-15 00:24:18 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
+  root: './',             // project root
+  build: {
+    outDir: 'dist',       // production build output
+    rollupOptions: {
+      input: '/index.html'  // entry point
+    }
+  }
 })
